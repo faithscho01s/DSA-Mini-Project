51fdf5397be127ffa8c8f96e55edfd35199e9d23
FaithDeVera <faithkirstendv14.21@ichat.sp.edu.sg>
2023-02-08 22:57:45 +0800

    added queue -faith

--- DSA_mini_proj2/DSA_mini_proj2.cpp
+++ DSA_mini_proj2/DSA_mini_proj2.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -15,6 +17,6 @@ int main()
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
+//   4. Use the Error List window to view errorssdfdsfsf
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
